619a988b4947434b3b339a5207f78673ad104acb
haihui_li <haihui_li@htc.com>
2014-06-25 13:26:59 +0800

    [code ]
    Symptom:
    Root Cause:
    Solution:
    Project:
    Note:

---
+++ File.h
@@ -0,0 +1,55 @@
+#ifndef __DIC_FILE_H__
+#define __DIC_FILE_H__
+
+typedef enum dict_file_type {
+    dict_stardict_info,
+    dict_stardict_index,
+    dict_stardict_dict,
+    dict_stardict_sync,
+}dict_file_type_t;
+
+
+class dictionary{
+
+}
+
+class stardict_dict :public dictionary{
+    public:
+
+
+    private:
+        file_opt finfo;
+        file_opt fdict;
+        file_opt fidx;
+        ifle_opt fsync;
+};
+
+
+
+
+const unsigned int MAX_PATH_LENGTH=1024;
+//implements genera file operations
+class file_opt {
+    public :
+        virtual void open(char *path,int mode);
+        virtual int read(char *buf,int len);
+        virtual int write(char *buf,int len);
+        virtual int lseek(int where,int offset);
+        virtual int readline(char *buf,int len);
+        virtual int check_file_type();
+    private:
+        int fd;
+        char file_path[MAX_PATH_LENGTH];
+};
+
+
+
+//using genera file operations to implements 
+//file opt interface
+class zipfile_opt:public file_opt {
+    static int check_file_type(char *buf,int len);
+};
+
+
+
+#endif 
