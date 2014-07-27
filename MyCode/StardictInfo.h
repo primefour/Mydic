7505bfe8e62ace2c6eedd393f18be4c70ff92df6
primefour <primefour@163.com>
2014-07-27 22:51:32 +0800

    restruct my code

---
+++ MyCode/StardictInfo.h
@@ -0,0 +1,97 @@
+#ifndef __STARDICT_INFO_H__
+#define __STARDICT_INFO_H__
+#define INFO_STRING_LENGTH 1024
+const char *ind_ver="version";
+const char *ind_name="bookname";
+const char *ind_count="wordcount";
+const char *ind_syn="synwordcount";
+const char *ind_size="idxfilesize";
+const char *ind_offset="idxoffsetbits";
+const char *ind_author="author";
+const char *ind_email="email";
+const char *ind_website="website";
+const char *ind_descrip="description";
+const char *ind_date="date";
+const char *ind_sts="sametypesequence";
+
+class StardictInfo{
+    public:
+        StardictInfo(const char *file_path):info_file(file_path){
+
+        }
+        ~StardictInfo(){
+            if(version){
+                free(version);
+            }
+            if(book_name){
+                free(book_name);
+            }
+            if(author){
+                free(author);
+            }
+            if(email){
+                free(email);
+            }
+            if(website){
+                free(website);
+            }
+            if(description){
+                free(description);
+            }
+            if(date){
+                free(date);
+            }
+        }
+        void init();
+        char *get_versoin(){
+            return version;
+        }
+        char *get_book_name(){
+            return book_name;
+        }
+        char *get_author(){
+            return author;
+        }
+        char *get_email(){
+            return email;
+        }
+        char *get_website(){
+            return website;
+        }
+        char *get_description(){
+            return description;
+        }
+        char *get_date(){
+            return date;
+        }
+        long get_word_count(){
+            return word_count;
+        }
+        long get_syn_word_count(){
+            return syn_word_count;
+        }
+        long get_idx_file_size(){
+            return idx_file_size;
+        }
+        int get_offset_bits(){
+            return  offset_is_64bit?64:32;
+        }
+        int get_same_types_sequence(){
+            return same_types_sequence;
+        }
+    private:
+        char *version;
+        char *book_name;
+        char *author;
+        char *email;
+        char *website;
+        char *description;
+        char *date;
+        long word_count;
+        long syn_word_count;
+        long idx_file_size;
+        short offset_is_64bit;
+        short same_types_sequence; 
+        File info_file;
+}
+#endif //
