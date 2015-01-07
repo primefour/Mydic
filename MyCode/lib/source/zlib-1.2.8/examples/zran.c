6ee53aa33246514aa9b8df3280438de32c4a7b02
haihui_li <haihui_li@htc.com>
2015-01-07 17:55:57 +0800

    [zran as lib]
    Symptom:
    Root Cause:
    Solution:
    Project:
    Note:

--- MyCode/lib/source/zlib-1.2.8/examples/zran.c
+++ MyCode/lib/source/zlib-1.2.8/examples/zran.c
@@ -56,27 +56,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "zlib.h"
-
-#define local static
-
-#define SPAN 1048576L       /* desired distance between access points */
-#define WINSIZE 32768U      /* sliding window size */
-#define CHUNK 16384         /* file input buffer size */
-
-/* access point entry */
-struct point {
-    off_t out;          /* corresponding offset in uncompressed data */
-    off_t in;           /* offset in input file of first full byte */
-    int bits;           /* number of bits (1-7) from byte at in - 1, or 0 */
-    unsigned char window[WINSIZE];  /* preceding 32K of uncompressed data */
-};
-
-/* access point list */
-struct access {
-    int have;           /* number of list entries filled in */
-    int size;           /* number of list entries allocated */
-    struct point *list; /* allocated list */
-};
+#include "zran.h"
 
 /* Deallocate an index built by build_index() */
 local void free_index(struct access *index)
