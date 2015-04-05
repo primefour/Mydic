ca84964835d47444ed63bf94b7b2da5823cd1380
haihui_li <haihui_li@htc.com>
2015-04-05 18:45:00 +0800

    Add native function define

--- DictApp2/app/src/main/java/com/Dict/DictApp2/DictSearchEngine.java
+++ DictApp2/app/src/main/java/com/Dict/DictApp2/DictSearchEngine.java
@@ -1,11 +1,34 @@
 package com.Dict.DictApp2;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
 /**
  * Created by crazyhorse on 15-4-5.
  */
 public class DictSearchEngine {
-    Callbacks mAsyncDeliverCB = null;
-    //public
+    private Callbacks mAsyncDeliverCB = null;
+    private static Handler mUIThreadHandler;
+    private final static String CALLBACK_SEARCH_RESULT = "searchResult";
+
+
+
+    public static native String engQueryWord(String searchWord);
+    public static native void engAsyncQueryWord(String searchWord);
+    public static native boolean engAddDictionary(String path);
+    public static native boolean engRemoveDictionary(String path);
+
+    public static void nativeCallback(String searchResult){
+        //will run at work thread
+        Bundle bundle = new Bundle();
+        bundle.putString(CALLBACK_SEARCH_RESULT,searchResult);
+        Message msg = Message.obtain();
+        msg.setData(bundle);
+        msg.setTarget(mUIThreadHandler);
+        msg.sendToTarget();
+    }
+
     public boolean addDictionary(String path){
         return false;
     }
@@ -16,8 +39,8 @@ public class DictSearchEngine {
         return null;
     }
 
-    public String asyncQueryWord(String searchWord){
-
+    public void asyncQueryWord(String searchWord){
+        return ;
     }
 
     public interface Callbacks {
