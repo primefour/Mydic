f8d8f1cdf9333b1c63b1d57f9048e42b32bd460f
primefour <primefour@163.com>
2015-04-13 21:02:53 +0800

    searchEngine jni compile OK

--- jni/Android.mk
+++ jni/Android.mk
@@ -12,19 +12,27 @@ LOCAL_SRC_FILES := src/com_Dict_DictApp2_DictSearchEngine.cpp \
 	src/StardictMain.cpp \
 	src/StardictSyn.cpp \
 	src/String8.cpp \
-	src/Unicode.cpp
+	src/Unicode.cpp \
+	src/adler32.c \
+	src/compress.c \
+	src/crc32.c \
+	src/deflate.c \
+	src/gzclose.c \
+	src/gzlib.c \
+	src/gzread.c \
+	src/gzwrite.c \
+	src/infback.c \
+	src/inffast.c \
+	src/inflate.c \
+	src/inftrees.c \
+	src/trees.c \
+	src/uncompr.c \
+	src/zutil.c \
 
 LOCAL_MODULE := searchEngine
 
 LOCAL_C_INCLUDES += ./include/
 
-LOCAL_LDFLAGS := -L ./lib/
-
-LOCAL_CFLAGS := -lz 
-
-#LOCAL_STATIC_LIBRARIES := libz
-
-LOCAL_LILIBS := -llog -lz
 
 include $(BUILD_SHARED_LIBRARY)
 #include $(BUILD_EXECUTABLE)
