9fa1c2f8d1fd6464dd3a168fd5adff921166bcb0
haihui_li <haihui_li@htc.com>
2015-09-01 18:03:58 +0800

    Add HTML example

--- DictApp2/app/src/main/java/com/Dict/DictApp2/MainFragment.java
+++ DictApp2/app/src/main/java/com/Dict/DictApp2/MainFragment.java
@@ -9,11 +9,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -26,10 +28,12 @@ public class MainFragment extends Fragment implements View.OnClickListener {
     private final String WEB_MIME_TYPE = "text/html";
     private final String NOT_FOUND_WORD = "Hello World!";
     private final String TAG = "MainFragment";
+    private Activity mMainActivity;
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
+        mMainActivity = activity;
 
         // Activities containing this fragment must implement its callbacks.
         if (!(activity instanceof Callbacks)) {
@@ -43,18 +47,64 @@ public class MainFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View view) {
 
+        /*
         if(getSearchWord() == null){
             return ;
         }
         String wordMeaning = mCallbacks.onSearchButtonClick(getSearchWord());
         if(wordMeaning == null){
             wordMeaning = NOT_FOUND_WORD;
-        }
+        }*/
         //show word Meaning
         WebView webView = ((WebView) mRootView.findViewById(R.id.MeaningWebView));
         webView.getSettings().setDefaultTextEncodingName(WEB_ENCODE_FORMAT);
-        webView.loadData(wordMeaning, "text/html; charset=UTF-8", null);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        final Activity activity = mMainActivity;
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                // Activities and WebViews measure progress with different scales.
+                // The progress meter will automatically disappear when we reach 100%
+                activity.setProgress(progress * 1000);
+            }
+        });
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Log.e(TAG,"######url = " + url);
+                return super.shouldOverrideUrlLoading(view,url);
+                //return true;
+            }
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+
+            public boolean shouldBlockMediaRequest(String url) {
+                Log.e(TAG,"######url = " + url);
+                return false ;
+            }
+
+            });
+        String wordMeaning ="<html>\n" +
+                "    <head>\n" +
+                "    <title>My Awesome Page</title>\n" +
+                "        <script type=\"text/javascript\" charset=\"utf-8\">\n" +
+                "            function sayHello() {\n" +
+                "                document.getElementById('foo').innerHTML = 'Hi there!';\n" +
+                "            }\n" +
+                "        </script>\n" +
+                "    </head>\n" +
+                "    <body>\n" +
+                "        <h1 id=\"foo\" onclick=\"sayHello()\">Click me!</h1>\n" +
+                "    </body>\n" +
+                "</html>\n" ;
+        //webView.loadData(wordMeaning, "text/html; charset=UTF-8", null);
+        //webView.loadDataWithBaseURL("file:///mnt/sdcard/",wordMeaning, "text/html","charset=UTF-8", null);
+        webView.loadDataWithBaseURL("file:///mnt/sdcard/",wordMeaning, "text/html","charset=UTF-8", null);
         //webView.loadData(wordMeaning,WEB_MIME_TYPE,WEB_ENCODE_FORMAT) ;
+        //webView.loadUrl("http://news.sina.com.cn/");
     }
 
     public interface Callbacks {
@@ -75,24 +125,12 @@ public class MainFragment extends Fragment implements View.OnClickListener {
         mWebView.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                /*
-                if (url.endsWith(".wav")){
-                    Log.d(TAG, "Reproducir archivo wav");
-                    Uri tempPath = Uri.parse(url);
-                    MediaPlayer player = MediaPlayer.create(getActivity(), tempPath);
-                    player.start();
-                    return true;
-                }else{
-                    Log.d(TAG, "Reproducir archivo xxxx" + url);
-                    return super.shouldOverrideUrlLoading(view, url);
-                }
-                */
-                MediaPlayer player = MediaPlayer.create(getActivity(), Uri.parse("file:///sdcard/hello.wav"));
-                player.start();
-                return true;
+                Log.e(TAG,"######url = " + url);
+                return super.shouldOverrideUrlLoading(view,url);
+                //return true;
             }
-
         });
+
         return mRootView;
     }
 
@@ -106,7 +144,8 @@ public class MainFragment extends Fragment implements View.OnClickListener {
     public void setWordMeaningString(String searchResult){
         WebView webView = ((WebView) mRootView.findViewById(R.id.MeaningWebView));
         webView.getSettings().setDefaultTextEncodingName(WEB_ENCODE_FORMAT);
-        webView.loadData(searchResult,WEB_MIME_TYPE,WEB_ENCODE_FORMAT) ;
+        //webView.loadData(searchResult,WEB_MIME_TYPE,WEB_ENCODE_FORMAT) ;
+        webView.loadUrl("http://news.sina.com.cn/");
     }
 
 
