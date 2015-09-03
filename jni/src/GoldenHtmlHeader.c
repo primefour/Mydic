1d76591ba0016bfbc9b06ecfa79e4f1f57ba274e
haihui_li <haihui_li@htc.com>
2015-09-03 17:35:58 +0800

    Add default html pattern

---
+++ jni/src/GoldenHtmlHeader.c
@@ -0,0 +1,7 @@
+const char *GoldenHtmlHeader= "<html>\n <head> \n <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0,maximum-scale=1.0\"> \n \
+    <title>Mi Guo Dictionary</title> \n </head> \n <style type=\"text/css\"> \n body { \n background-position: center top; \n color: #665544; \n background-color: #d4d0c6; \n \
+    } \n p { \n background-image: url(\"img2.jpg\");} \n .title { \n font-family:Helvetica, Arial, sans-serif; \n background-image: url(\"img3.jpg\"); \n font-size:100%; \n color: #055550; \n \
+    margin: 10px 0px 5px 0px; \n padding: 0px; \n } \n .title1 { \n font-family:Helvetica, Arial, sans-serif; \n font-size:130%; \n color: #077770; \n margin: 10px 0px 5px 0px; \n padding: 0px;\n\
+    }\n .meaning{ \n font-family:Helvetica, Arial, sans-serif; \n font-size:100%; \n color: #332211; \n margin: 10px 0px 5px 0px; \n padding: 0px; \n } \n img.large { \n width: 500px; \n \
+	 height: 500px;}\n img.medium { \n width: 250px;\n height: 250px;}\n img.small {\n width: 100px;\n height: 100px;}\n img.align-left { \n float: left; \n margin-right: 10px;} \n \
+     img.align-center { \n display: block;\n margin: 0px auto;} \n img.align-right	{ \n float: right;\n margin-left: 10px;}\n </style> \n <body> \n" ;
