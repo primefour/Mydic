866c21b667e4117dd9e4632306b14a82b95eaef5
haihui_li <haihui_li@htc.com>
2015-09-21 18:18:04 +0800

    fix bug

--- jni/src/GoldenDictManager.cpp
+++ jni/src/GoldenDictManager.cpp
@@ -15,35 +15,40 @@ const char *ImgPath = "/sdcard/GoldenDict/Img/";
 void GoldenDictManager::doWithFiles(const char *file){
     golden_printfe("file = %s \n",file);
     String8 tmp_path(file);
-    golden_printfd("getFullExtension %s \n",tmp_path.getFullExtension().string());
+    //golden_printfd("getFullExtension %s \n",tmp_path.getFullExtension().string());
     if(tmp_path.getFullExtension().string() == NULL){
         return ;
     }
     String8 Ext(tmp_path.getFullExtension());
-    golden_printfd("##%s \n",Ext.string());
+    //golden_printfd("##%s \n",Ext.string());
     Ext.toLower();
-    golden_printfd("%s \n",Ext.string());
+    //golden_printfd("%s \n",Ext.string());
 
     int type = mDictionaryType[Ext];
 
-    golden_printfe("type %d  \n",type);
+    //golden_printfe("type %d  \n",type);
     if(type == 0){
         return ;
     }
 
-    SObject<GoldenDictInterface> obj = mDictionaryMap[String8(tmp_path.getBasePath())];
-    if(obj.GetPoint() != NULL ){
-        golden_printfd("mDictionaryMap is NULL \n");
-        return ;
-    }
-    golden_printfd("mDictionaryMap tmp_path.getBasePath() %s \n",tmp_path.getBasePath().string());
 
     if(type == STAR_DICT_TYPE){
         try{
             //get the type of dictionary by the extend of file
             GoldenDictInterface *tmp =  new StardictInstance(tmp_path.getBasePath());
+
             golden_printfe("dictionary name = %s \n",tmp->GetDictonaryName().string());
-            mDictionaryMap[String8(tmp_path.getBasePath())] = tmp; 
+
+            SObject<GoldenDictInterface> obj = mDictionaryMap[tmp->GetDictonaryName()];
+            if(obj.GetPoint() != NULL ){
+                golden_printfd("mDictionaryMap is not NULL \n");
+                delete tmp;
+                return ;
+            }else{
+                golden_printfd("mDictionaryMap is NULL \n");
+            }
+            golden_printfd("mDictionaryMap tmp_path.getBasePath() %s \n",tmp_path.getBasePath().string());
+            mDictionaryMap[tmp->GetDictonaryName()] = tmp;
         }catch(exception &ex){
             golden_printfe("add dict fail %s %s  \n",tmp_path.string(),ex.what());
         }
