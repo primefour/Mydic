e720c3326ef2e9e6ca8b0039bb35385e3bf720de
primefour <primefour@163.com>
2016-02-14 23:09:08 +0800

    update Dict header

--- DictVersion2/jni/inc/AngelDict.h
+++ DictVersion2/jni/inc/AngelDict.h
@@ -2,17 +2,32 @@
 #define __ANGEL_DICT_H__
 
 
+/*
+ *  {
+ *      word:"hello",
+ *      phonetic:[{
+ *      value:"[helou]",url:"file:///sdcard/voice/hello.mp3",type:1,
+ *      }]
+ *      meaning:[{text:"ni hao",ex:"Hello,My dear",pic:[
+ *      url:"file:///scard/pic/hello.jpg",res:176x324,comment:meeting
+ *      ]
+ *      }
+ *      ]
+ *  }
+ *
+ */
+
 const char *DictJsonObject[] = {
     "word","phonetic","meaning"
 };
 
 
 const char *DictPhoneticField[]={
-    "url","type"
+    "value","url","type"
 };
 
 const char *DictMeaningField[]={
-    "text","example","picture"
+    "text","ex","pic"
 };
 
 const char *DictPictFeild[]={
@@ -20,5 +35,12 @@ const char *DictPictFeild[]={
 };
 
 
+class AngelDictWord{
+    public:
+        void AddPict(const char *url,int w,int h,const char *comment);
+        void AddMeaning(const char *text,const char *ex);
+        void AddPhonetic(const char *value,const char *url,int type);
+    private:
+};
 
 #endif //__ANGEL_DICT_H__
