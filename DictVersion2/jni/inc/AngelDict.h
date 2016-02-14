702470dd777b95c7704c3f120127bef825743402
haihui_li <haihui_li@htc.com>
2016-02-14 16:54:16 +0800

    Add Angel header

---
+++ DictVersion2/jni/inc/AngelDict.h
@@ -0,0 +1,24 @@
+#ifndef __ANGEL_DICT_H__
+#define __ANGEL_DICT_H__
+
+
+const char *DictJsonObject[] = {
+    "word","phonetic","meaning"
+};
+
+
+const char *DictPhoneticField[]={
+    "url","type"
+};
+
+const char *DictMeaningField[]={
+    "text","example","picture"
+};
+
+const char *DictPictFeild[]={
+    "url","res","comment"
+};
+
+
+
+#endif //__ANGEL_DICT_H__
